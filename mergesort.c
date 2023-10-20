f4b8f17ff4bbd87e0bf87796eb1929dafd1655be
tejasvi <tejasvigelani@gmail.com>
2023-10-20 11:09:26 +0530

    20/10/23

--- mergesort.c
+++ mergesort.c
@@ -5,42 +5,42 @@
 void merge(int a[],int l, int mid, int h)
 {
     int b[100];
-    int i=1, j=mid+1, k=l;
+    int i=l, j=mid+1, k=l;
     while(i<=mid && j<=h)
     {
         if(a[i]<a[j])
-        b[k++]=a[i++];
+            b[k++]=a[i++];
         else
-        b[k++]=a[j++];
+            b[k++]=a[j++];
     }
     while(i<=mid)
-    b[k++]=a[i++];
+        b[k++]=a[i++];
     while(j<=h)
-    b[k++]=a[j++];
-    for(i=1; i<=h; i++)
-    a[i]=b[i];
+        b[k++]=a[j++];
+    for(i=l; i<=h; i++)
+        a[i]=b[i];
 }
 int mergesort(int a[],int l,int h)
 {
     int mid;
     if(l<h){
         mid=(l+h)/2;
-        mergesort(a,l,mid);
-        mergesort(a,mid+1,h);
-       merge(a,l,mid,h);
+          mergesort(a,l,mid);
+          mergesort(a,mid+1,h);
+          merge(a,l,mid,h);
     }
 }
 int main()
 {
     int a[15],n=15,i;
     for(i=0; i<n; i++)
-    a[i]=rand()%99+1;
-    printf("before sorting array is:\n");
+      a[i]=rand()%99+1;
+          printf("before sorting array is:\n");
     for(i=0; i<n; i++)
-    printf("%d ",a[i]);
+          printf("%d ",a[i]);
 
     mergesort(a,0,n-1);
-    printf("\n after sorting array is:\n");
+        printf("\n after sorting array is:\n");
     for(i=0; i<n; i++)
-    printf("%d ",a[i]);
+        printf("%d ",a[i]);
 }
